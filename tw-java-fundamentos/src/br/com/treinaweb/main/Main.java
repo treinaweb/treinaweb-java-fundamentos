1ddb04f6dec5add0fc30c1805dbdeb0831f914c9
Cleber Campomori <cleber.campomori@gmail.com>
2018-04-19 19:16:59 -0300

    Aula 6.1: Estruturas de repetição - while

--- tw-java-fundamentos/src/br/com/treinaweb/main/Main.java
+++ tw-java-fundamentos/src/br/com/treinaweb/main/Main.java
@@ -4,55 +4,47 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		/*System.out.print("Digite uma mensagem: ");
-		String mensagem = scanner.next();
-		String mensagem = scanner.nextLine();
-		System.out.println(mensagem);*/
-		// int numero1 = Integer.parseInt(scanner.nextLine());
-		System.out.print("Digite o primeiro número: ");
-		int numero1 = scanner.nextInt();	
-		System.out.print("Digite a operação: ");
-		char operacao = scanner.next().charAt(0);
-		System.out.print("Digite o segundo número: ");
-		int numero2 = scanner.nextInt();
-		// Você quer fazer a operação 9 + 2
-		System.out.println(String.format("Você quer fazer a operação %d %c %d", numero1, operacao, numero2));
-		int resultado = 0;
-//		if (operacao == '+') {
-//			resultado = numero1 + numero2;
-//		} else if (operacao == '-') {
-//			resultado = numero1 - numero2;
-//		} else if (operacao == '*') {
-//			resultado = numero1 * numero2;
-//		} else if (operacao == '/') {
-//			resultado = numero1 / numero2;
-//		} else {
-//			System.out.println("Operação inválida.");
-//		}
-		switch (operacao) {
-		case '+':
-			resultado = numero1 + numero2;
-			break;
-		case '-':
-			resultado = numero1 - numero2;
-			break;
-		case '*':
-			resultado = numero1 * numero2;
-			break;
-		case '/':
-			resultado = numero1 / numero2;
-			break;
-		default:
-			System.out.println("Operação inválida");
-		}
+		int acao = 1; // 0 = sair; 1 = operação matemática; 2 = histórico
 		ArrayList<String> historico = new ArrayList<String>();
-		String entradaHistorico = String.format("%d %c %d = %d", numero1, operacao, numero2, resultado);
-		historico.add(entradaHistorico);
-		System.out.println(entradaHistorico);
-		scanner.close();		
+		while (acao > 0) {			
+			if (acao == 1) {
+				System.out.println(" === Operação matemática ===");
+				System.out.print("Digite o primeiro número: ");
+				int numero1 = scanner.nextInt();
+				System.out.print("Digite a operação: ");
+				char operacao = scanner.next().charAt(0);
+				System.out.print("Digite o segundo número: ");
+				int numero2 = scanner.nextInt();
+				System.out.println(String.format("Você quer fazer a operação %d %c %d", numero1, operacao, numero2));
+				int resultado = 0;
+				switch (operacao) {
+				case '+':
+					resultado = numero1 + numero2;
+					break;
+				case '-':
+					resultado = numero1 - numero2;
+					break;
+				case '*':
+					resultado = numero1 * numero2;
+					break;
+				case '/':
+					resultado = numero1 / numero2;
+					break;
+				default:
+					System.out.println("Operação inválida");
+				}				
+				String entradaHistorico = String.format("%d %c %d = %d", numero1, operacao, numero2, resultado);
+				historico.add(entradaHistorico);
+				System.out.println(entradaHistorico);
+			} else if (acao == 2) {
+				System.out.println(" === Histórico ===");
+				System.out.println(String.format("- Você já fez %d operações", historico.size()));
+			}
+		}
+		scanner.close();
 	}
 
 }
