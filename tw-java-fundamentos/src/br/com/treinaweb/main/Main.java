d2bbc9b464788cbe56b19a4d5861d7fd3c5410fb
Cleber Campomori <cleber.campomori@gmail.com>
2018-04-19 18:53:37 -0300

    Aula 1.3: Criando a primeira classe e o primeiro package

---
+++ tw-java-fundamentos/src/br/com/treinaweb/main/Main.java
@@ -0,0 +1,10 @@
+package br.com.treinaweb.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Olá, mundo!");
+	}
+
+}
