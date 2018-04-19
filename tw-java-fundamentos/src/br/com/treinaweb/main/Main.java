c7c687cde6490b3b4bd424f6bb119f0180fddda0
Cleber Campomori <cleber.campomori@gmail.com>
2018-04-19 18:59:02 -0300

    Aula 3.3: convertendo tipos de dados

--- tw-java-fundamentos/src/br/com/treinaweb/main/Main.java
+++ tw-java-fundamentos/src/br/com/treinaweb/main/Main.java
@@ -6,12 +6,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("Digite seu nome: ");
-		String nome = scanner.nextLine();
-		System.out.println("Olá, mundo!");
-		System.out.print("Olá mundo de novo \n");
-		System.out.println("Olá mundo mais uma vez");
-		System.out.println(nome);
+		System.out.print("Digite o primeiro número: ");
+		int numero1 = Integer.parseInt(scanner.nextLine());
+		System.out.println(numero1);		
 		scanner.close();
 	}
 
