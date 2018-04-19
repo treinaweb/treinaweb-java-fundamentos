065a611f87021dd7481d1c0b8b82b119d2fc2bfc
Cleber Campomori <cleber.campomori@gmail.com>
2018-04-19 19:02:42 -0300

    Aula 3.5: lidando com chars e entendendo a diferença entre next() e nextLine() no scanner

--- tw-java-fundamentos/src/br/com/treinaweb/main/Main.java
+++ tw-java-fundamentos/src/br/com/treinaweb/main/Main.java
@@ -6,10 +6,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		/*System.out.print("Digite uma mensagem: ");
+		String mensagem = scanner.next();
+		String mensagem = scanner.nextLine();
+		System.out.println(mensagem);*/
 		System.out.print("Digite o primeiro número: ");
 		// int numero1 = Integer.parseInt(scanner.nextLine());
-		int numero1 = scanner.nextInt();		
-		System.out.println(numero1);		
+		int numero1 = scanner.nextInt();	
+		System.out.print("Digite a operação: ");
+		char operacao = scanner.next().charAt(0);
 		scanner.close();
 	}
 
