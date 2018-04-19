2ff9e23317d2ce4df65c7d0e0655b5df776f8415
Cleber Campomori <cleber.campomori@gmail.com>
2018-04-19 19:08:33 -0300

    Aula 3.6: Explorando o scanner e entendendo o compiler compliance

--- tw-java-fundamentos/src/br/com/treinaweb/main/Main.java
+++ tw-java-fundamentos/src/br/com/treinaweb/main/Main.java
@@ -10,11 +10,15 @@ public class Main {
 		String mensagem = scanner.next();
 		String mensagem = scanner.nextLine();
 		System.out.println(mensagem);*/
-		System.out.print("Digite o primeiro número: ");
 		// int numero1 = Integer.parseInt(scanner.nextLine());
+		System.out.print("Digite o primeiro número: ");
 		int numero1 = scanner.nextInt();	
 		System.out.print("Digite a operação: ");
 		char operacao = scanner.next().charAt(0);
+		System.out.print("Digite o segundo número: ");
+		int numero2 = scanner.nextInt();
+		// Você quer fazer a operação 9 + 2
+		System.out.println(String.format("Você quer fazer a operação %d %c %d", numero1, operacao, numero2));
 		scanner.close();
 	}
 
